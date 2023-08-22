7f10602f60b93e1b67def115eecea667cbb3eae8
Javier Albero <splash.creeper1789@gmail.com>
2023-08-22 13:48:56 +0200

    Database connection and status

---
+++ src/connection_manager.rs
@@ -0,0 +1,60 @@
+use postgres::{Client, NoTls};
+use std::env::VarError;
+use std::env::var;
+use dotenv::{from_path};
+
+
+///Specifies the variable name for the database's user
+const VAR_USER:&str = "DB_USER";
+///Specifies the variable name for the database's password
+const VAR_PASSWORD:&str = "DB_PASSWORD";
+///Specifies the variable name for the database's host
+const VAR_HOST:&str = "DB_HOST";
+///Specifies the variable name for the database's name
+const VAR_NAME:&str = "DB_NAME";
+
+fn get_variable_name_by_index(index:&usize) -> &str {
+    match index {
+        0 => {VAR_USER}
+        1 => {VAR_PASSWORD}
+        2 => {VAR_HOST}
+        3 => {VAR_NAME}
+        _ => {"NONE"}
+    }
+}
+pub fn get_envs(path:&str) -> (Result<String, VarError>, Result<String, VarError>, Result<String, VarError>, Result<String, VarError>) {
+    //!It obtains from a path the four environment variables or an error.
+    match from_path(path) {
+        Ok(()) => {
+            return (var(VAR_USER), var(VAR_PASSWORD), var(VAR_HOST), var(VAR_NAME));
+        }
+        Err(e) => {
+            println!("Couldn't find the environment variables file: {}", e);
+            return (var(VAR_USER), var(VAR_PASSWORD), var(VAR_HOST), var(VAR_NAME));
+        }
+    }
+}
+
+fn get_connection (user: String, password: String, host:String, name: String) -> Result<Client, postgres::Error> {
+    let url = format!("postgres://{user}:{password}@{host}/{name}");
+    return Client::connect(url.as_str(), NoTls);
+}
+
+pub fn connect(data: (Result<String, VarError>, Result<String, VarError>, Result<String, VarError>, Result<String, VarError>)) -> Result<Client, postgres::Error> {
+    let mut credentials: Vec<String> = vec![];
+    for (i, d) in [data.0, data.1, data.2, data.3].iter().enumerate() {
+        match d {
+            Ok(c) => {credentials.push(c.to_string())}
+            Err(e) => {
+                println!("An error has ocurred while obtaining variable {}: {e}", get_variable_name_by_index(&i));
+                credentials.push(format!(""));
+            }
+        }
+    }
+    get_connection(
+        credentials.get(0).unwrap().to_string(),
+        credentials.get(1).unwrap().to_string(),
+        credentials.get(2).unwrap().to_string(),
+        credentials.get(3).unwrap().to_string()
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
+mod connection_manager;
+
 #[macro_use] extern crate rocket;
 
+use std::env;
+use std::error::Error;
+use std::ops::{Deref, DerefMut};
+use std::sync::Mutex;
+use rocket::State;
+use postgres::{Client};
+use crate::connection_manager::get_envs;
+
+struct AppState {
+    db_client: Mutex<Result<Client, postgres::Error>>,
+}
+
+
+struct quote_post {
+    quote:String,
+    sign:String,
+}
 
 #[get("/health")]
-fn health(name: &str, age: u8) -> String {
-    format!("status: OK")
+fn health(state: &State<AppState>) -> String {
+    let mut db_status = String::default();
+    match state.db_client.lock().unwrap().deref() {
+        Ok(_) => {
+            db_status = format!("db_connection: \"OK\"");
+        }
+        Err(e) => {
+            db_status = format!("db_connection: \"{}\"", e);
+        }
+    }
+    format!("status: \"OK\"\n{db_status}")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![health])
+    println!("{:?}", env::current_dir().unwrap());
+    let conn = connection_manager::connect(get_envs(".env"));
+    let conn_state = AppState {db_client: Mutex::new(conn)};
+    rocket::build().manage(conn_state).mount("/", routes![health])
 }
