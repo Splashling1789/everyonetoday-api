244238b7cb1fac9cd2f2cfff10f5464870e8e1bb
Javier Albero <splash.creeper1789@gmail.com>
2023-08-22 13:49:33 +0200

    Changed date to timestamp in the database

--- sql/init-tables.sql
+++ sql/init-tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "quotes"(
                                        "id" SERIAL,
-                                       "date" DATE NOT NULL,
+                                       "date" TIMESTAMP NOT NULL,
                                        "sign" VARCHAR(20) NOT NULL,
                                        "quote" TEXT NOT NULL
 );
