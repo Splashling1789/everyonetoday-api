db39e69dd428efbed1e6124a85967ee9f64cd100
Javier Albero <splash.creeper1789@gmail.com>
2023-08-30 15:38:11 +0200

    Changed response from route health to JSON

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,22 +2,27 @@ use std::ops::Deref;
 use rocket::State;
 use std::sync::Mutex;
 use postgres::{Client};
-use rocket_db_pools::{Connection, Database, sqlx};
+use rocket_db_pools::{Database, sqlx};
+use rocket_db_pools::sqlx::Connection;
+use rocket::serde::json::{json, Value};
 
 #[derive(Database)]
 #[database("main_db")]
 pub struct MainDb(sqlx::PgPool);
 
 #[get("/health")]
-pub async fn health(mut db: Connection<MainDb>) -> String {
+pub async fn health(mut db: rocket_db_pools::Connection<MainDb>) -> Value {
     let mut db_status = String::default();
-    match sqlx::query("SELECT * FROM quotes LIMIT 1;").fetch_one(&mut *db).await {
-        Ok(_) => {
-            db_status = format!("db_connection: \"OK\"");
+    match db.ping().await {
+        Ok(()) => {
+            db_status = format!("OK");
         }
         Err(e) => {
-            db_status = format!("db_connection: \"{}\"", e);
+            db_status = format!("{e}");
         }
     }
-    format!("status: \"OK\"\n{db_status}")
+    json!({
+        "status":"OK",
+        "db_status":db_status
+    })
 }
