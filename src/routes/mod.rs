ee82c312e35a21c03814a347f4eace14ac69cd25
Javier Albero <splash.creeper1789@gmail.com>
2023-08-31 16:55:04 +0200

    health route improved

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
-use rocket::serde::json::{json, Value};
+use rocket::serde::{json::{json, Value}, Serialize};
+use rocket::serde::json::Json;
 use rocket_db_pools::sqlx::Connection;
 use rocket_db_pools::{sqlx, Database};
 
@@ -6,8 +7,21 @@ use rocket_db_pools::{sqlx, Database};
 #[database("main_db")]
 pub struct MainDb(sqlx::PgPool);
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct StatusResponse {
+    status: String,
+    db_status: DbStatus,
+}
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct DbStatus {
+    ping: String,
+    version: Option<u32>,
+}
+
 #[get("/health")]
-pub async fn health(mut db: rocket_db_pools::Connection<MainDb>) -> Value {
+pub async fn health(mut db: rocket_db_pools::Connection<MainDb>) -> Json<StatusResponse> {
     let db_status = match db.ping().await {
         Ok(()) => {
             format!("OK")
@@ -16,8 +30,12 @@ pub async fn health(mut db: rocket_db_pools::Connection<MainDb>) -> Value {
             format!("{e}")
         }
     };
-    json!({
-        "status": "OK",
-        "db_status": db_status
+
+    Json(StatusResponse {
+        status: format!("OK"),
+        db_status: DbStatus {
+            ping: db_status,
+            version: db.server_version_num()
+        }
     })
 }
