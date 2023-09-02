e19d99fd5ee7e260e468d90bb98249ed57bdd735
splashling <splashling1789@gmail.com>
2023-09-02 21:16:31 +0200

    Prevented sending signs with line breaks

--- src/routes/post_write.rs
+++ src/routes/post_write.rs
@@ -25,19 +25,35 @@ pub async fn write(
     mut db: Connection<MainDb>,
     post: Json<PostWriteData>,
 ) -> Json<PostWriteResponse> {
+    if has_line_breaks(post.sign.clone()) {
+        return Json(PostWriteResponse {
+            success: false,
+            error_output: Some(format!("API ERROR: linebreaks are not allowed in the 'sign' field")),
+        });
+    }
     match query(QUERY_WRITE)
         .bind(post.sign.clone())
         .bind(post.quote.clone())
         .execute(&mut *db)
         .await
     {
-        Ok(q) => Json(PostWriteResponse {
+        Ok(_) => Json(PostWriteResponse {
             success: true,
             error_output: None,
         }),
         Err(e) => Json(PostWriteResponse {
             success: false,
-            error_output: Some(format!("{e}")),
+            error_output: Some(format!("DB ERROR {e}")),
         }),
     }
+}
+
+
+fn has_line_breaks(content: String) -> bool {
+    for c in content.chars() {
+        if c == 0xA as char  {
+            return true;
+        }
+    }
+    return false;
 }
