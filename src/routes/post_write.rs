ac1cd1985b454c9505b67acb0303853b1ed9a1c9
Javier Albero <splash.creeper1789@gmail.com>
2023-09-04 15:18:40 +0200

    Same sign cannot be used twice in a day now

--- src/routes/post_write.rs
+++ src/routes/post_write.rs
@@ -4,7 +4,7 @@ use rocket::serde::{Deserialize, Serialize};
 use rocket_db_pools::Connection;
 use sqlx::query;
 
-const QUERY_WRITE: &str = "INSERT INTO quotes VALUES (DEFAULT, now(), $1, $2);";
+const QUERY_WRITE: &str = "INSERT INTO quotes (\"date\", \"sign\", \"quote\") SELECT NOW(), $1, $2 WHERE NOT EXISTS (SELECT 1 FROM quotes WHERE date::date = NOW()::date AND \"sign\"=$3);";
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -28,19 +28,31 @@ pub async fn write(
     if has_line_breaks(post.sign.clone()) {
         return Json(PostWriteResponse {
             success: false,
-            error_output: Some(format!("API ERROR: linebreaks are not allowed in the 'sign' field")),
+            error_output: Some(format!(
+                "API ERROR: linebreaks are not allowed in the 'sign' field"
+            )),
         });
     }
     match query(QUERY_WRITE)
         .bind(post.sign.clone())
         .bind(post.quote.clone())
+        .bind(post.sign.clone())
         .execute(&mut *db)
         .await
     {
-        Ok(_) => Json(PostWriteResponse {
-            success: true,
-            error_output: None,
-        }),
+        Ok(q) => {
+            if q.rows_affected() == 0 {
+                Json(PostWriteResponse {
+                    success: false,
+                    error_output: Some(format!("API ERROR: that sign has alredy been used today")),
+                })
+            } else {
+                Json(PostWriteResponse {
+                    success: true,
+                    error_output: None,
+                })
+            }
+        }
         Err(e) => Json(PostWriteResponse {
             success: false,
             error_output: Some(format!("DB ERROR {e}")),
@@ -48,10 +60,9 @@ pub async fn write(
     }
 }
 
-
 fn has_line_breaks(content: String) -> bool {
     for c in content.chars() {
-        if c == 0xA as char  {
+        if c == 0xA as char {
             return true;
         }
     }
